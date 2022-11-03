7d7d9eb516bb2493051057aef58a681bc5622d5b
Nikola <nikola.malinov.krastev@gmail.com>
2022-11-03 12:14:53 +0200

    Updated HRT8

--- Introduction/src/HRT8_JavaDatatypes.java
+++ Introduction/src/HRT8_JavaDatatypes.java
@@ -29,3 +29,18 @@ public class HRT8_JavaDatatypes {
         }
     }
 }
+/*Java has 8 primitive data types; char, boolean, byte, short, int, long, float, and double.
+For this exercise, we'll work with the primitives used to hold integer values (byte, short, int, and long):
+A byte is an 8-bit signed integer.
+A short is a 16-bit signed integer.
+An int is a 32-bit signed integer.
+A long is a 64-bit signed integer.
+Given an input integer, you must determine which primitive data types are capable of properly storing that input.
+To get you started, a portion of the solution is provided for you in the editor.
+Reference: https://docs.oracle.com/javase/tutorial/java/nutsandbolts/datatypes.html
+Input Format:
+The first line contains an integer n , denoting the number of test cases.
+Each test case n, is comprised of a single line with an integer number, which can be arbitrarily large or small.
+Output Format:
+For each input variable number and appropriate primitive dataType, you must determine if the given primitives
+are capable of storing it. If yes, then print:*/
