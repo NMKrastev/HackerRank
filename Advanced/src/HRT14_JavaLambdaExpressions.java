499b6becf9566c594cad8cb1f52040f753339d57
Nikola <nikola.malinov.krastev@gmail.com>
2022-11-03 15:27:45 +0200

    Added HRT14

--- Advanced/src/HRT14_JavaLambdaExpressions.java
+++ Advanced/src/HRT14_JavaLambdaExpressions.java
@@ -30,7 +30,6 @@ class MyMath {
 public class HRT14_JavaLambdaExpressions {
 
     public static void main(String[] args) throws IOException {
-        MyMath ob = new MyMath();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int T = Integer.parseInt(br.readLine());
         PerformOperation op;
@@ -42,18 +41,17 @@ public class HRT14_JavaLambdaExpressions {
             int ch = Integer.parseInt(st.nextToken());
             int num = Integer.parseInt(st.nextToken());
             if (ch == 1) {
-                op = ob.isOdd();
-                ret = ob.checker(op, num);
+                op = MyMath.isOdd();
+                ret = MyMath.checker(op, num);
                 ans = (ret) ? "ODD" : "EVEN";
             } else if (ch == 2) {
-                op = ob.isPrime();
-                ret = ob.checker(op, num);
+                op = MyMath.isPrime();
+                ret = MyMath.checker(op, num);
                 ans = (ret) ? "PRIME" : "COMPOSITE";
             } else if (ch == 3) {
-                op = ob.isPalindrome();
-                ret = ob.checker(op, num);
+                op = MyMath.isPalindrome();
+                ret = MyMath.checker(op, num);
                 ans = (ret) ? "PALINDROME" : "NOT PALINDROME";
-
             }
             System.out.println(ans);
         }
