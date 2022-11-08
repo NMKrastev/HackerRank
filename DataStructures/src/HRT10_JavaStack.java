dc27ebfad6af8fba6e5ccf95d4fd7987e3e53839
Nikola Krastev <nikola.malinov.krastev@gmail.com>
2022-11-08 09:22:05 +0200

    Added HRT10

---
+++ DataStructures/src/HRT10_JavaStack.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+import java.util.Stack;
+
+public class HRT10_JavaStack {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        while (scanner.hasNextLine()) {
+            Stack<Character> characterStack = new Stack<>();
+            String line = scanner.nextLine();
+
+            for (char character : line.toCharArray()) {
+
+                if (character == '(' || character == '[' || character == '{') {
+                    characterStack.push(character);
+                    continue;
+                }
+                if (character == ')' && !characterStack.isEmpty() && characterStack.peek() == '(') {
+                    characterStack.pop();
+                    continue;
+                }
+                if (character == ']' && !characterStack.isEmpty() && characterStack.peek() == '[') {
+                    characterStack.pop();
+                    continue;
+                }
+                if (character == '}' && !characterStack.isEmpty() && characterStack.peek() == '{') {
+                    characterStack.pop();
+                    continue;
+                }
+                if (character == ')' || character == ']' || character == '}') {
+                    characterStack.push(character);
+                    break;
+                }
+            }
+            System.out.println(characterStack.isEmpty());
+        }
+    }
+}
+/*In computer science, a stack or LIFO (last in, first out) is an abstract data type that serves as a collection of
+elements, with two principal operations: push, which adds an element to the collection, and pop, which removes the
+last element that was added.(Wikipedia)
+A string containing only parentheses is balanced if the following is true: 1. if it is an empty string 2. if A and B
+are correct, AB is correct, 3. if A is correct, (A) and {A} and [A] are also correct.
+Examples of some correctly balanced strings are: "{}()", "[{()}]", "({()})"
+Examples of some unbalanced strings are: "{}(", "({)}", "[[", "}{" etc.
+Given a string, determine if it is balanced or not.
+Input Format:
+There will be multiple lines in the input file, each having a single non-empty string. You should read input till end-of-file.
+The part of the code that handles input operation is already provided in the editor.
+Output Format:
+For each case, print 'true' if the string is balanced, 'false' otherwise.*/
